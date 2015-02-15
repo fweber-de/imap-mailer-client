7083ee5f7cd8b92414965c500c1ffab4cf69fe0e
Florian Weber <florian.weber.dd@icloud.com>
2015-02-15 21:16:55 +0100

    app

---
+++ src/Mailer/AppBundle/Controller/AppController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mailer\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AppController extends Controller
+{
+    public function unifiedInboxAction()
+    {
+        return $this->render("App/unified.html.twig");
+    }
+
+    public function settingsAction()
+    {
+        return $this->render("App/settings.html.twig");
+    }
+
+    public function sidebarAction($accountId = null)
+    {
+        if (!$accountId) {
+            $user = $this->getUser();
+
+            $accounts = $this->getDoctrine()->getRepository('MailerDataBundle:Account')->findByUser($user);
+
+            return $this->render(
+                '_sidebar.html.twig',
+                array(
+                    'accounts' => $accounts
+                )
+            );
+        }
+
+        $account = $this->getDoctrine()->getRepository('MailerDataBundle:Account')->findOneById($accountId);
+
+        if (!$account) {
+            throw $this->createNotFoundException(sprintf('No Account with ID %s was found!', $accountId));
+        }
+
+        return $this->render(
+            '_sidebar.html.twig',
+            array(
+                'account' => $account
+            )
+        );
+    }
+}
